ec57f164f8fc6e97b2ac16a686cc4c933e24e8f9
deih <paluch.jakub0@gmail.com>
2015-06-29 23:31:37 +0200

    Ability to create new job

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,43 @@
+class JobsController < ApplicationController
+	before_action :find_job, except: [:index, :new, :create]
+
+	def index
+		@jobs = Job.all.order('created_at DESC')
+	end
+
+	def show
+	end
+
+	def new
+		@job = Job.new
+	end
+
+	def create
+		@job = Job.new(jobs_params)
+
+		if @job.save
+			redirect_to @job
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+	private
+
+	def find_job
+		@job = Job.find(params[:id])
+	end
+
+	def jobs_params
+		params.require(:job).permit(:title, :description, :company, :url)
+	end
+end
